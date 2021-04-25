6b45a452f55cb9f3dcb8996f6d73827018f2f21d
laelacitrasih <laelacitrasih@gmail.com>
2021-04-26 04:41:14 +0700

    Update client

--- client/gallery-application/src/app/app.module.ts
+++ client/gallery-application/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './gallery/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    WelcomeComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
